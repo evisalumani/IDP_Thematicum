98374bf88cb04cc953ca93434af949e9298e42a3
Evisa Lumani <evisa.lumani@tum.de>
2018-04-18 00:48:08 +0200

    Add screenshots

--- server/utilities/registrationAccessCodes.js
+++ server/utilities/registrationAccessCodes.js
@@ -2,11 +2,11 @@ db.registrationaccesscodes.insert([
 {
 	code: 'THEMATICUM16TUM',
 	validFrom: new Date().getTime(),
-	validUntil: new Date(2017, 4, 1).getTime()
+	validUntil: new Date(2020, 4, 1).getTime()
 },
 {
 	code: 'THEMATICUMIDPTUM',
 	validFrom: new Date().getTime(),
-	validUntil: new Date(2017, 4, 1).getTime()
+	validUntil: new Date(2020, 4, 1).getTime()
 }
 ]);
